bbebd2896f73b142f828188aab48b0175545c0bf
Andrew On <a_on93@hotmail.com>
2023-11-15 11:25:30 -0500

    update getAllReservations function/query

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -76,7 +76,25 @@ const addUser = function (user) {
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  const result = `SELECT properties.*, reservations.*, avg(rating) as average_rating
+  FROM reservations
+  JOIN properties ON reservations.property_id = properties.id
+  JOIN property_reviews ON properties.id = property_reviews.property_id
+  WHERE reservations.guest_id = $1
+  AND reservations.end_date < now()::date
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date
+  LIMIT $2`;
+  const values = [guest_id, limit];
+  return pool.query(result, values)
+  .then(res => {
+    if (res.rows) {
+      return res.rows;
+    } else {
+      return null;
+    }
+  })
+  .catch(err => console.log('query error:', err));
 };
 
 /// Properties
